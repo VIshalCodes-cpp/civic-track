5787f0fa47ac9025c5081b623e72cf2978ca2588
VIshalCodes-cpp <vishalrawat1402@gmail.com>
2026-05-11 01:40:54 +0530

    'm

--- client/firebase.tsx
+++ client/firebase.tsx
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyASjHoThDqoT-kp5EUpGL4s8-r0UncTOYY",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "civictrack-cd12f.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "civictrack-cd12f",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "civictrack-cd12f.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "281097746888",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:281097746888:web:c35dca9f664495b073e8fd",
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-LPYT6RPLNM"
 };
 
 // Initialize Firebase
