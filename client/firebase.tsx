59e7100b507f8e6ce4df20f46126f7b0d628b9c3
VIshalCodes-cpp <vishalrawat1402@gmail.com>
2026-05-11 19:34:37 +0530

    Added API Key

--- client/firebase.tsx
+++ client/firebase.tsx
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyASjHoThDqoT-kp5EUpGL4s8-r0UncTOYY",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "civictrack-cd12f.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "civictrack-cd12f",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "civictrack-cd12f.firebasestorage.app",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "281097746888",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:281097746888:web:c35dca9f664495b073e8fd",
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-LPYT6RPLNM"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyDh4qWe1J79z6yVbVGjRDOfY3-OR_kvL8A",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "project-1-b8b69.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "project-1-b8b69",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "project-1-b8b69.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "48174289786",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:48174289786:web:4285217626fc6c730bbcc3",
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-3W9PBF71NE"
 };
 
 // Initialize Firebase
